f8d3617238fb82954e6ee988d71ab43f1ffc0e10
Jason Smith (air) <jhs@iriscouch.com>
2012-02-19 10:52:20 +0700

    Treat a CORS failure as an error, the callback sets er.cors = "rejected" to indicate why

--- src/request.js
+++ src/request.js
@@ -177,6 +177,19 @@ function run_xhr(options) {
     LOG.debug('Got response', {'id':xhr.id, 'status':xhr.status})
     clearTimeout(xhr.timeoutTimer)
     xhr.statusCode = xhr.status // Node request compatibility
+
+    // Detect failed CORS requests.
+    if(is_cors && xhr.statusCode == 0) {
+      var cors_err = new Error('CORS request rejected: ' + options.uri)
+      cors_err.cors = 'rejected'
+
+      // Do not process this request further.
+      did.loading = true
+      did.end = true
+
+      return options.callback(cors_err, xhr)
+    }
+
     options.onResponse(null, xhr)
   }
 
